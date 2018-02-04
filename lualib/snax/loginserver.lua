9804d7df7af0c0e4faa8a834805f9d19cc27fc1d
yuqangy@qq.com <yuqangy@qq.com>
2018-02-04 23:39:09 +0800

    1

--- lualib/snax/loginserver.lua
+++ lualib/snax/loginserver.lua
@@ -64,8 +64,8 @@ local function launch_slave(auth_handler, register_handler)
 			return _ and ok, code
 			
 		elseif opcode == "l" then
-			--local challenge = crypt.randomkey()
-			local challenge = test_challenge
+			local challenge = crypt.randomkey()
+			--local challenge = test_challenge
 			print("challenge", challenge)
 			write("auth", fd, crypt.base64encode(challenge).."\n")
 
@@ -76,8 +76,8 @@ local function launch_slave(auth_handler, register_handler)
 			end
 			print("clientkey", clientkey)
 			
-			--local serverkey = crypt.randomkey()
-			local serverkey = test_serverkey
+			local serverkey = crypt.randomkey()
+			--local serverkey = test_serverkey
 			serverkey = crypt.dhexchange(serverkey)
 			print("serverkey", serverkey)
 			write("auth", fd, crypt.base64encode(serverkey).."\n")
