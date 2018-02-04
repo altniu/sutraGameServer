51c4598841b88a967c3ac42ca4dc778f00f70a10
yuqangy@qq.com <yuqangy@qq.com>
2018-02-04 20:09:43 +0800

    1

---
+++ server/gameonce/create_table.sql
@@ -0,0 +1,49 @@
+use  sutraGameDB;
+
+drop table userBaseData;
+
+create table userBaseData
+    (
+    uuid char(64) not null primary key,
+    registerTime long not null,
+    jingtuGroup char(64) not null,
+	lotusNum int not null,
+	phoneType char(32) default '',
+	userData varchar(32),
+	unique(uuid)
+	);
+
+	
+	
+	
+
+drop table userUpdateData;
+
+create table userUpdateData
+    (
+    uuid char(64) not null primary key,
+	incenseLastTime int not null,
+	sutraLastTime int not null,
+	signNum int not null,
+	censerNum int not null,
+	sutraNum int not null,
+	signRank int not null,
+	censerRank int not null,
+	sutraRank int not null,
+	totalRank int not null,
+	unique(uuid)
+	);
+	
+	
+	
+	
+	drop table monthCollect;
+
+create table monthCollect
+	(
+	uuid char(64) not null  primary key,
+	signLine int not null,
+	month int not null,
+	fohaoGroup varchar(64),
+	unique(uuid)
+	);
