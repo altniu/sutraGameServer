fb0e13db0f021d941c505976ea0ebc570cb2c109
yuqangy@qq.com <yuqangy@qq.com>
2018-03-20 22:18:36 +0800

    1

--- server/gameonce/game/agentPlayer.lua
+++ server/gameonce/game/agentPlayer.lua
@@ -279,7 +279,7 @@ function REQUEST:totalPush()
 		local scores = split(r.fohaoGroup, ",")
 		for k,v in pairs(scores) do
 			local s = split(v, ":")
-			print(s)
+			printTable(s)
 			printTable(r)
 			printTable(pinfo.musicScore)
 			pinfo.musicScore[s[1]] = tonumber(s[2])
